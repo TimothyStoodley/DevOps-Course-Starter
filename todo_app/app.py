2e544cbf9e18f01748851aacc22ba765ab637590
Timothy Stoodley <timothy.stoodley@uk.zurich.com>
2021-04-09 17:42:38 +0100

    Exercise 1 project completed and committed.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+
+from todo_app.data.session_items import get_item, get_items, add_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -8,8 +10,14 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items=items)
 
+@app.route('/add-item', methods=['POST'])
+def create_item():
+    item = request.form.get('item')
+    add_item(item)
+    return redirect('/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
